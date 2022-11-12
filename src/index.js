0b58f5545f2b7857008edd07e2496c87c9eb29d6
Katsute <58778985+Katsute@users.noreply.github.com>
2022-11-12 12:49:09 -0500

    optimize selector

--- src/index.js
+++ src/index.js
@@ -16,8 +16,9 @@
     if(window.location.pathname.toLowerCase().includes("/projects/"))
         whenElementAdded(`#__primerPortalRoot__`, (e) => {
             e.addEventListener("DOMNodeInserted", (i) => {
-                if(i.target.querySelector("figure > span > svg"))
-                    i.target.querySelector(`path[d="M11.28 6.78a.75.75 0 00-1.06-1.06L7.25 8.69 5.78 7.22a.75.75 0 00-1.06 1.06l2 2a.75.75 0 001.06 0l3.5-3.5z"]`).parentElement.parentElement.classList.add("rissue-closed-bg");
+                let svg;
+                if(svg = i.target.querySelector("figure > span > svg"))
+                    svg.querySelector(`path[d="M11.28 6.78a.75.75 0 00-1.06-1.06L7.25 8.69 5.78 7.22a.75.75 0 00-1.06 1.06l2 2a.75.75 0 001.06 0l3.5-3.5z"]`).parentElement.parentElement.classList.add("rissue-closed-bg");
             });
         });
 
