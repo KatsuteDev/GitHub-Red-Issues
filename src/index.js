69184200b031bb5e72af5559c3cc976782b157fc
Katsute <58778985+Katsute@users.noreply.github.com>
2023-03-05 12:50:57 -0500

    fix init listener, fix commits being colored wrong

--- src/index.js
+++ src/index.js
@@ -27,8 +27,9 @@
 
         if(window.location.pathname.toLowerCase() === "/search"){
             const apply = (e) => {
-                for(const path of e.querySelectorAll(`path[d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Zm1.5 0a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Zm10.28-1.72-4.5 4.5a.75.75 0 0 1-1.06 0l-2-2a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018l1.47 1.47 3.97-3.97a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042Z"]`))
-                    path.parentElement.classList.add("rissue-closed-fg");
+                if(window.location.search.includes("type=issues"))
+                    for(const path of e.querySelectorAll(`path[d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Zm1.5 0a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Zm10.28-1.72-4.5 4.5a.75.75 0 0 1-1.06 0l-2-2a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018l1.47 1.47 3.97-3.97a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042Z"]`))
+                        path.parentElement.classList.add("rissue-closed-fg");
             };
 
             whenElementAdded(`react-app[app-name="blackbird-search"]`, (e) => {
@@ -39,5 +40,5 @@
     };
 
     init();
-    window.addEventListener("popstate", init);
+    window.addEventListener("turbo:visit", init);
 })();
