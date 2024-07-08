e784abb3f122fb0b5aa5b2eaadd360babf64b1d9
Katsute <58778985+Katsute@users.noreply.github.com>
2024-07-07 22:24:55 -0400

    simplify tests

--- test.js
+++ test.js
@@ -1,73 +1,50 @@
-const assert = require("assert");
 const puppeteer = require("puppeteer");
 
+const URL = "https://github.com/KatsuteDev/GitHub-Red-Issues";
+
+const variables = [
+    `--bgColor-closed-emphasis`,
+    `--color-danger-emphasis`,
+    `--fgColor-closed`,
+    `--color-danger-fg`,
+    `--bgColor-closed-muted`,
+    `--color-danger-subtle`,
+];
+
+const selectors = [
+
+];
+
 (async () => {
-    const URL = "https://github.com/KatsuteDev/GitHub-Red-Issues";
-
-    // init puppeteer
-
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(URL);
 
-    // css
-    const variables = [
-        `--bgColor-closed-emphasis`,
-        `--color-danger-emphasis`,
-        `--fgColor-closed`,
-        `--color-danger-fg`,
-        `--bgColor-closed-muted`,
-        `--color-danger-subtle`,
-    ];
-
-    // test
-
-    const variableExists = async (v) => {
-        return await page.evaluate((v2) => {
-            const style = getComputedStyle(document.documentElement);
-            return style.getPropertyValue(v2) !== '';
-        }, v);
-    };
-
     let failed = false;
+
     for(const v of variables){
-        try{
-            assert(await variableExists(v));
+        if(
+            await page.evaluate((v2) => {
+                const style = getComputedStyle(document.documentElement);
+                return style.getPropertyValue(v2) !== '';
+            }, v)
+        ){
             console.info('✅', v);
-        }catch(e){
+        }else{
             console.error('❌', v);
             failed = true;
         }
     }
 
-    // selectors
-
-    const selectorExists = async (selector) => {
-        try{
-            await page.waitForSelector(selector, { timeout: 5000 });
-            return true;
-        }catch(e){
-            return false;
-        }
-    };
-
-    const selectors = [
-
-    ];
-
-    // test
-
     for(const s of selectors){
         try{
-            assert(await selectorExists(s));
+            await page.waitForSelector(s, { timeout: 5000 });
             console.info('✅', s);
         }catch(e){
             console.error('❌', s);
             failed = true;
         }
     }
-
-    // post
 
     await browser.close();
 
